9db360ba35c7cf7633d71b15d0fed26fa3d35f2e
Jimmy <jimmycolema@google.com>
2020-07-01 21:06:08 +0000

    Decouple AppBar with page content

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,17 +1,41 @@
+// React Framework imports
 import * as React from "react";
 import { hot } from "react-hot-loader";
+import { BrowserRouter, Route, Switch, Link} from "react-router-dom";
+import clsx from 'clsx';
+
+// Material-UI Imports
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
 
 // Component Imports
-import PersistentDrawerLeft from "./drawer/Drawer"
+import AppBar from "./appbar/AppBar";
+
+// Page Imports
+import { Explore } from "./pages/explore/Explore";
+import { Login } from "./pages/login/Login"; 
+import { Profile } from "./pages/profile/Profile";
+import { YourClubs } from "./pages/your_clubs/YourClubs";
+
+const drawerWidth = 240;
 
 class App extends React.Component<{}, undefined> {
-    public render() {
-        return (
-          <PersistentDrawerLeft />
-        );
-    }
+
+  public render() {
+    return (
+      <BrowserRouter>
+        <AppBar />
+        <main>
+          <Switch>
+            <Route exact path="/Login" component={Login} />
+            <Route exact path="/YourClubs" component={YourClubs} />
+            <Route exact path="/Explore" component={Explore} />
+            <Route exact path="/Profile" component={Profile} />
+          </Switch>
+        </main>
+      </BrowserRouter>
+    );
+  }
 }
 
 declare let module: object;
-
 export default hot(module)(App);
