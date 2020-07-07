b03afd1d20427958807e968293dd02d758881356
Joey Bushagour <jbushagour@google.com>
2020-07-07 17:08:17 +0000

    Add class to encapsulate a book club.

---
+++ server/src/main/java/com/google/coffeehouse/common/Club.java
@@ -0,0 +1,171 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.coffeehouse.common;
+
+import com.google.coffeehouse.util.IdentifierGenerator;
+import com.google.coffeehouse.util.UuidWrapper;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Club encapsulates the information associated with each book club.
+ * 
+ * <p>Implementes the {@link Saveable} interface because it will be able to save itself
+ * in the database.
+ */
+public class Club implements Saveable {
+  private String name;
+  private Book currentBook;
+  private String clubId;
+  private String description;
+  private List<String> contentWarnings;
+
+  private Club(Builder builder) {
+    this.name = builder.name;
+    this.currentBook = builder.currentBook;
+    this.clubId = builder.idGenerator.generateId();
+    this.description = builder.description;
+    this.contentWarnings = builder.contentWarnings;
+  }
+
+  /** Overloaded static factory, calls other fromMap with null IdentifierGenerator. */
+  public static Club fromMap(Map clubInfo) {
+    return Club.fromMap(clubInfo, null);
+  }
+
+  /** 
+   * Creates a {@link Club} object from a Map with the relevant parameters as keys.
+   * @param clubInfo the Map that contains the information used to construct the Club. At a 
+   *     minimum this includes a {@code "name"} key that corresponds to the String describing the
+   *     name of the club, as well as a {@code "currentBook"} key that is mapped to another Map
+   *     that follows the format described in {@link Book#fromMap(Map, IdentifierGenerator)}.
+   *     Optional keys include a {@code "description"} key that maps to a String describing the 
+   *     Club's description, as well as a {@code "contentWarnings"} key that maps to a List of 
+   *     Strings describing the content warnings of the Club. Suitable defaults will be created
+   *     in the absence of these optional keys
+   * @param idGen the {@link IdentifierGenerator} used when constructing the Club and Book. If
+   *     null, the default generator will be used
+   * @return the created Club
+   * @throws IllegalArgumentException if no valid {@code "name"} key or 
+   *     {@code "currentBook"} key is defined
+   */
+  public static Club fromMap(Map clubInfo, IdentifierGenerator idGen) {
+    String name = (String) clubInfo.getOrDefault("name", null);
+    Map bookInfo = (Map) clubInfo.getOrDefault("currentBook", null);
+    if (name == null || bookInfo == null) {
+      throw new IllegalArgumentException();
+    }
+
+    Book currentBook = Book.fromMap(bookInfo, idGen);
+    Club.Builder clubBuilder = 
+        Club.newBuilder(name, currentBook)
+            .setContentWarnings((List) clubInfo.getOrDefault("contentWarnings", new ArrayList<>()))
+            .setDescription((String) clubInfo.getOrDefault("description", ""));
+    if (idGen != null) {
+      clubBuilder.setIdGenerator(idGen);
+    }
+    return clubBuilder.build();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Book getCurrentBook() {
+    return currentBook;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public List<String> getContentWarnings() {
+    return contentWarnings;
+  }
+
+  public String getClubId() {
+    return clubId;
+  }
+
+  public void setBook(Book book) {
+    // TODO: add the old book to book history @linamontes10
+    this.currentBook = book;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public void setContentWarnings(List<String> contentWarnings) {
+    this.contentWarnings = contentWarnings;
+  }
+
+  /** Starts the building process of a new Club object. */
+  public static Builder newBuilder(String name, Book book) {
+    return new Builder(name, book);
+  }
+
+  @Override
+  public String toString() {
+    String clubInfo = String.format("name: %s, description: %s, contentWarnings: %s, clubId: %s",
+                                    name, description, contentWarnings, clubId);
+    String bookInfo = currentBook.toString();
+    return clubInfo + ", currentBook: (" + bookInfo + ")";
+  }
+
+  @Override
+  public void save() {
+    // TODO: implement saving @linamontes10
+  }
+
+  public static class Builder {
+    private String name;
+    private Book currentBook;
+    private String description = "";
+    private IdentifierGenerator idGenerator = new UuidWrapper();
+    private List<String> contentWarnings = null;
+    private static final String DEFAULT_DESCRIPTION = "A book club about %s.";
+
+    public Builder(String name, Book currentBook) {
+      this.name = name;
+      this.currentBook = currentBook;
+    }
+
+    public Builder setDescription(String description) {
+      this.description = description;
+      return this;
+    }
+
+    public Builder setContentWarnings(List<String> contentWarnings) {
+      this.contentWarnings = contentWarnings;
+      return this;
+    }
+
+    public Builder setIdGenerator(IdentifierGenerator idGenerator) {
+      this.idGenerator = idGenerator;
+      return this;
+    }
+
+    private String generateDefaultDescription() {
+      return String.format(DEFAULT_DESCRIPTION, currentBook.getTitle());
+    }
+
+    public Club build() {
+      description = description == "" ? generateDefaultDescription() : description;
+      return new Club(this);
+    }
+  }
+}
---
+++ server/src/test/java/com/google/coffeehouse/common/ClubTest.java
@@ -0,0 +1,156 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.coffeehouse.common;
+
+import com.google.coffeehouse.util.IdentifierGenerator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.function.ThrowingRunnable;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+/**
+ * Unit tests for {@link Club}.
+ */
+@RunWith(JUnit4.class)
+public final class ClubTest {
+  private static final String NAME = "Club Name";
+  private static final String DESCRIPTION = "Club Description";
+  private static final String ALT_DESCRIPTION = "New Club Description";
+  private static final String IDENTIFICATION_STRING = "predetermined-identification-string";
+  private static final String BOOK_TITLE = "Book Name";
+  private static final String ALT_BOOK_TITLE = "New Book Name";
+  private List<String> testContentWarnings;
+  private Club.Builder clubBuilder;
+  private Book testBook;
+  private Book altTestBook;
+  private Map clubInfo;
+  private Map bookInfo;
+
+  @Mock private IdentifierGenerator idGen;
+
+  @Before
+  public void beforeTest() {
+    testContentWarnings = new ArrayList<>(Arrays.asList("1", "2"));
+
+    clubInfo = new HashMap();
+    bookInfo = new HashMap<String, String>();
+    bookInfo.put("title", BOOK_TITLE);
+    
+    idGen = Mockito.mock(IdentifierGenerator.class);
+    Mockito.when(idGen.generateId()).thenReturn(IDENTIFICATION_STRING);
+
+    testBook = Book.newBuilder(BOOK_TITLE).build();
+    altTestBook = Book.newBuilder(ALT_BOOK_TITLE).build();
+    clubBuilder = Club.newBuilder(NAME, testBook);
+  }
+
+  @Test
+  public void getExistingName() {
+    Club c = clubBuilder.build();
+    Assert.assertEquals(NAME, c.getName());
+  }
+
+  @Test
+  public void getExistingDescription() {
+    Club c = clubBuilder.setDescription(DESCRIPTION).build();
+    Assert.assertEquals(DESCRIPTION, c.getDescription());
+  }
+
+  @Test
+  public void getExistingCurrentBook() {
+    Club c = clubBuilder.build();
+    Assert.assertEquals(testBook, c.getCurrentBook());
+  }
+
+  @Test
+  public void getExistingClubId() {
+    Club c = clubBuilder.setIdGenerator(idGen).build();
+    Assert.assertEquals(IDENTIFICATION_STRING, c.getClubId());
+  }
+
+  @Test
+  public void getExistingContentWarnings() {
+    Club c = clubBuilder.setContentWarnings(testContentWarnings).build();
+    Assert.assertEquals(testContentWarnings, c.getContentWarnings());
+  }
+  
+  @Test
+  public void setNewContentWarnings() {
+    Club c = clubBuilder.setContentWarnings(testContentWarnings).build();
+    c.setContentWarnings(new ArrayList<>());
+    Assert.assertEquals(new ArrayList<>(), c.getContentWarnings());
+  }
+
+  @Test
+  public void setNewDescription() {
+    Club c = clubBuilder.build();
+    c.setDescription(ALT_DESCRIPTION);
+    Assert.assertEquals(ALT_DESCRIPTION, c.getDescription());
+  }
+
+  @Test
+  public void setNewBook() {
+    Club c = clubBuilder.build();
+    c.setBook(altTestBook);
+    Assert.assertEquals(altTestBook, c.getCurrentBook());
+  }
+  
+  @Test 
+  public void fromInvalidMap() {
+    clubInfo.put("contentWarnings", testContentWarnings);
+    Assert.assertThrows(IllegalArgumentException.class, new ThrowingRunnable() {
+        @Override
+        public void run() throws Throwable {
+          Club.fromMap(clubInfo);
+        }
+    });
+  }
+
+  @Test 
+  public void fromMinimumValidMap() {
+    clubInfo.put("name", NAME);
+    clubInfo.put("currentBook", bookInfo);
+    Club c = Club.fromMap(clubInfo);
+    Assert.assertEquals(NAME, c.getName());
+    Assert.assertEquals(new ArrayList<>(), c.getContentWarnings());
+    Assert.assertEquals(BOOK_TITLE, c.getCurrentBook().getTitle());
+  }
+
+  @Test 
+  public void fromMaximumValidMap() {
+    clubInfo.put("name", NAME);
+    clubInfo.put("contentWarnings", testContentWarnings);
+    clubInfo.put("description", DESCRIPTION);
+    clubInfo.put("currentBook", bookInfo);
+
+    Club c = Club.fromMap(clubInfo, idGen);
+    Assert.assertEquals(NAME, c.getName());
+    Assert.assertEquals(testContentWarnings, c.getContentWarnings());
+    Assert.assertEquals(BOOK_TITLE, c.getCurrentBook().getTitle());
+    Assert.assertEquals(DESCRIPTION, c.getDescription());
+    Assert.assertEquals(IDENTIFICATION_STRING, c.getClubId());
+  }
+
+  // TODO: test saving @linamontes10
+}
