9eef69eae966995cea5a9254e7ca0d5a08cf1088
Jimmy <jimmycolema@google.com>
2020-07-07 16:59:47 +0000

    Button to handle submit

--- frontend/src/components/pages/profile/Profile.tsx
+++ frontend/src/components/pages/profile/Profile.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Button from "@material-ui/core/Button";
 import { createStyles } from "@material-ui/core/styles";
 import FilledInput from "@material-ui/core/FilledInput";
 import FormControl from "@material-ui/core/FormControl";
@@ -32,68 +33,92 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function Profile() {
   const [email, setEmail] = React.useState('Email');
-  const [name, setName] = React.useState('Name');
+  const [nickname, setNickname] = React.useState('Name');
   const [pronouns, setPronouns] = React.useState('Pronouns');
 
   const classes = useStyles();
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
+    console.log(email);
   };
 
   const handleNicknameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
+    setNickname(event.target.value);
+    console.log(nickname);
   };
 
   const handlePronounsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPronouns(event.target.value);
+    console.log(pronouns)
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const Person = {};
+    console.log(nickname);
+    console.log(pronouns);
+    console.log(email);
   };
 
   return (
     <div className={classes.root}>
-      <div>
-        <TextField
-          fullWidth
-          helperText="to be displayed around CoffeeHouse"
-          id="nickname"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          label="Nickname"
-          margin="normal"
-          onChange={handleNicknameChange}
-          placeholder="Nickname"
-          style={{ margin: 8 }}
-          variant="outlined"
-        />
-        <TextField
-          fullWidth
-          helperText="to be displayed around CoffeeHouse"
-          id="pronouns"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          label="Pronouns"
-          margin="normal"
-          onChange={handlePronounsChange}
-          placeholder="Pronouns"
-          style={{ margin: 8 }}
-          variant="outlined"
-        />
-        <TextField
-          fullWidth
-          id="email"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          label="Email"
-          margin="normal"
-          onChange={handleEmailChange}
-          placeholder="Email"
-          style={{ margin: 8 }}      
-          variant="outlined"
-        />
-      </div>
+      <form>
+        <div>
+          <TextField
+            fullWidth
+            helperText="to be displayed around CoffeeHouse"
+            id="nickname"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            label="Nickname"
+            margin="normal"
+            onChange={handleNicknameChange}
+            placeholder="Nickname"
+            style={{ margin: 8 }}
+            variant="outlined"
+          />
+          <TextField
+            fullWidth
+            helperText="to be displayed around CoffeeHouse"
+            id="pronouns"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            label="Pronouns"
+            margin="normal"
+            onChange={handlePronounsChange}
+            placeholder="Pronouns"
+            style={{ margin: 8 }}
+            variant="outlined"
+          />
+          <TextField
+            fullWidth
+            id="email"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            label="Email"
+            margin="normal"
+            onChange={handleEmailChange}
+            placeholder="Email"
+            style={{ margin: 8 }}      
+            variant="outlined"
+          />
+        </div>
+        <div>
+          <Button 
+            color="primary" 
+            onClick={handleSubmit}
+            style={{ margin: 8 }} 
+            variant="contained" 
+          >
+            Submit
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
