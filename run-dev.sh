5d7354b6a0b093d020090f53bc030049155ce341
Joey Bushagour <jbushagour@google.com>
2020-07-14 16:39:25 +0000

    Switch order of backend/frontend dev server instantiation for consistent, smaller line length.

--- run-dev.sh
+++ run-dev.sh
@@ -9,5 +9,5 @@ then
 fi
 
 trap "kill %1" SIGINT
-(cd frontend/; npm run start-dev) | tee frontend.log | sed -e "s/^/\x1b[32m[FRONTEND]\x1b[0m /" \
-& (cd server/; mvn package appengine:run) | tee server.log | sed -e "s/^/\x1b[35m[BACKEND]\x1b[0m /"
+(cd server/; mvn package appengine:run) | tee server.log | sed -e "s/^/\x1b[35m[BACKEND]\x1b[0m /" \
+& (cd frontend/; npm run start-dev) | tee frontend.log | sed -e "s/^/\x1b[32m[FRONTEND]\x1b[0m /"
