45179136ce7b07a467ca0394639063332646b199
Joey Bushagour <jbushagour@google.com>
2020-07-17 20:54:21 +0000

    Add interface for the backend authentication.

--- frontend/src/services/backend_service_interface/backend_service_interface.ts
+++ frontend/src/services/backend_service_interface/backend_service_interface.ts
@@ -1,10 +1,14 @@
 export interface BackendProfileServiceInterface {
-  loadProfile(id: string): Promise<string>;
-  updateProfile(personJson: string): Promise<boolean>;
-  deleteProfile(id: string): Promise<boolean>;
+  loadProfile(id: string): Promise<string>,
+  updateProfile(personJson: string): Promise<boolean>,
+  deleteProfile(id: string): Promise<boolean>,
 }
 
 export interface BackendYourClubsServiceInterface {
-  listClubs(numClubs: number): Promise<string>;
-  leaveClub(clubId: string): Promise<boolean>;
+  listClubs(numClubs: number): Promise<string>,
+  leaveClub(id: string): Promise<boolean>,
+}
+
+export interface BackendAuthenticationInterface {
+  retrieveToken(code: string, redirectUri: string): Promise<string>,
 }
