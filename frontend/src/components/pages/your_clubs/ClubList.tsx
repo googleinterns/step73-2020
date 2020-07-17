04af3de9f60316f30c00dd34d6a51e0c22b48093
Jimmy <jimmycolema@google.com>
2020-07-17 17:09:49 +0000

    Alphabetize imports.

--- frontend/src/components/pages/your_clubs/ClubList.tsx
+++ frontend/src/components/pages/your_clubs/ClubList.tsx
@@ -4,17 +4,16 @@ import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import { ClubProps } from "../../../services/mock_backend/mock_your_clubs_backend";
 import { createStyles } from "@material-ui/core/styles";
-import HighlightOffIcon from "@material-ui/icons/HighlightOff";
-import LibraryBooksRoundedIcon from "@material-ui/icons/LibraryBooksRounded";
-import { makeStyles } from "@material-ui/core/styles";
-import PageviewIcon from "@material-ui/icons/Pageview";
-import { Theme } from "@material-ui/core/styles";
-
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
+import LibraryBooksRoundedIcon from "@material-ui/icons/LibraryBooksRounded";
+import { makeStyles } from "@material-ui/core/styles";
+import PageviewIcon from "@material-ui/icons/Pageview";
+import { Theme } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
