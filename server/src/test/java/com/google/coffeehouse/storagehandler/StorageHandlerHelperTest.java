2a84f613b03f21e0adc3f047e2af3da5208e8651
Lina Montes <monteslina54@gmail.com>
2020-07-24 18:35:47 +0000

    Create new tests to verify that a current member can't become an owner

--- server/src/test/java/com/google/coffeehouse/storagehandler/StorageHandlerHelperTest.java
+++ server/src/test/java/com/google/coffeehouse/storagehandler/StorageHandlerHelperTest.java
@@ -46,6 +46,16 @@ public class StorageHandlerHelperTest {
   }
 
   @Test
+  public void checkAnyMembership_memberFailsToBecomeOwner() throws Exception {
+    StorageHandlerTestHelper.insertPerson("person");
+    StorageHandlerTestHelper.insertClubWithContentWarnings("club");
+    StorageHandlerTestHelper.insertMembership("person", "club", MembershipConstants.MEMBER);
+    assertThrows(RuntimeException.class, () -> {
+      StorageHandlerTestHelper.insertMembership("person", "club", MembershipConstants.OWNER);;
+    });
+  }
+
+  @Test
   public void checkAnyMembership_personInClub() throws Exception {
     StorageHandlerTestHelper.insertPerson("person");
     StorageHandlerTestHelper.insertClub("club");
