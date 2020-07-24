6f44de66a248db610505c7f5606280cf89b057f5
Jimmy <jimmycolema@google.com>
2020-07-24 13:44:51 +0000

    Create person in Spanner if their profile does not exist.

--- frontend/src/components/pages/login/Login.tsx
+++ frontend/src/components/pages/login/Login.tsx
@@ -32,13 +32,31 @@ export const Login = (props: LoginProps) => {
 
   const classes = useStyles();
 
-  // Fires upon login success
-  const tokenConsumer = (token: string) => {
+  // Fires upon login success.
+  const tokenConsumer = async (token: string) => {
     // Cache token in localStorage to keep user signed in after refresh.
     localStorage.setItem('token', token);
     loginStatusHandlerService.setUserToken(token);
     loginStatusHandlerService.setUserLoginStatus(/*Successfully logged in*/ true);
     props.handleUserLogin();
+
+    // Determine if user profile exists; if not, create it.
+    const getProfileURL = "/api/get-profile?" + new URLSearchParams({idToken: token});
+    const getProfileResponse = await fetch(getProfileURL, {
+      method: "GET",
+    });
+    if (getProfileResponse.status != 200) {
+      const parsedToken = JSON.parse(atob(token.split('.')[1]));
+      const person = {
+        nickname: parsedToken.name,
+        email: parsedToken.email,
+        userId: parsedToken.sub,
+      }
+      const createPersonResponse = await fetch("/api/create-person", {
+        method: "POST",
+        body: JSON.stringify(person),
+      });
+    }
   }
 
   const failureCallback = () => {
