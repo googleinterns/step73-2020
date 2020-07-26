6beec08b326ecc39fa495b048bbf19e071434f5f
Joey Bushagour <jbushagour@google.com>
2020-07-26 06:59:44 +0000

    Update interface for club calls.

--- frontend/src/services/backend_service_interface/backend_service_interface.ts
+++ frontend/src/services/backend_service_interface/backend_service_interface.ts
@@ -5,19 +5,19 @@ export interface PersonInterface {
   pronouns?: string,
 }
 
-export interface IBook {
+export interface BookInterface {
   bookId: string,
   title: string,
   author: string,
   isbn?: string,
 }
 
-export interface IClub {
+export interface ClubInterface {
   clubId: string,
   ownerId: string,
   contentWarnings: string[],
   description: string,
-  currentBook: IBook,
+  currentBook: BookInterface,
 }
 
 export interface BackendProfileServiceInterface {
@@ -28,9 +28,12 @@ export interface BackendProfileServiceInterface {
 }
 
 export interface BackendYourClubsServiceInterface {
-  createClub(clubJson: string): Promise<boolean>,
-  listClubs(numClubs: number): Promise<string>,
-  leaveClub(clubId: string): Promise<boolean>,
+  createClub(ClubInterface: string): Promise<ClubInterface>,
+  listClubs(
+    token: string,
+    membership: "member" | "not member"): Promise<ClubInterface[]>,
+  leaveClub(clubId: string, token: string): Promise<boolean>,
+  joinClub(clubId: string, token: string): Promise<boolean>,
 }
 
 export interface BackendAuthenticationInterface {
