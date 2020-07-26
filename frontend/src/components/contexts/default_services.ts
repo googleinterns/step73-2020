3c43fc8b0ebc291326c5f6dafc8fe667aafde90c
Joey Bushagour <jbushagour@google.com>
2020-07-26 06:32:45 +0000

    Make the real profile backend the default service.

--- frontend/src/components/contexts/default_services.ts
+++ frontend/src/components/contexts/default_services.ts
@@ -1,20 +1,20 @@
 import { AuthenticationHandlerService } from "../../services/authentication_handler_service/authentication_handler_service";
 import { AuthenticationBackendService } from "../../services/backend/authentication_backend";
 import { LoginStatusHandlerService } from "../../services/login_status_handler_service/login_status_handler_service";
-import { MockProfileBackendService } from "../../services/mock_backend/mock_profile_backend";
 import { MockYourClubsBackendService } from "../../services/mock_backend/mock_your_clubs_backend";
 import { NUM_PROFILES } from "../../utils/temporary_testing_consts";
+import { ProfileBackendService } from "../../services/backend/profile_backend";
 import { ProfileHandlerService } from "../../services/profile_handler_service/profile_handler_service";
 import { YourClubsHandlerService } from "../../services/your_clubs_handler_service/your_clubs_handler_service";
 
 /** TODO: Change to real backend service implementation. */
-export const mockProfileBackendService = new MockProfileBackendService(NUM_PROFILES);
+export const profileBackendService = new ProfileBackendService();
 export const mockYourClubsBackendService = new MockYourClubsBackendService();
-export const actualAuthenticationBackendService = new AuthenticationBackendService();
+export const authenticationBackendService = new AuthenticationBackendService();
 
 export const defaultServices = {
-  profileHandlerService: new ProfileHandlerService(mockProfileBackendService),
+  profileHandlerService: new ProfileHandlerService(profileBackendService),
   yourClubsHandlerService: new YourClubsHandlerService(mockYourClubsBackendService),
-  authenticationHandlerService: new AuthenticationHandlerService(actualAuthenticationBackendService),
+  authenticationHandlerService: new AuthenticationHandlerService(authenticationBackendService),
   loginStatusHandlerService: new LoginStatusHandlerService(),
 }
