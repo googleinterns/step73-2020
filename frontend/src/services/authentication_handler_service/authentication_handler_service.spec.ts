1a05a81e8640c99777118aa819739e75216ed131
Joey Bushagour <jbushagour@google.com>
2020-07-29 07:16:46 +0000

    Rewrite auth handler service to handle login / session through gapi Auth2.

--- frontend/src/services/authentication_handler_service/authentication_handler_service.spec.ts
+++ frontend/src/services/authentication_handler_service/authentication_handler_service.spec.ts
@@ -1,7 +1,12 @@
 import { AuthenticationHandlerService, FailureToSignInError } from "./authentication_handler_service";
 
 // load these objects to overwrite gapi calls.
-let authInstance = {};
+let authInstance = {
+  isSignedIn: {
+    listen: jest.fn(),
+  },
+  signOut: jest.fn(),
+};
 window.gapi = {
   load: jest.fn(),
   auth2: {
@@ -11,6 +16,8 @@ window.gapi = {
 
 const EXPECTED_SCOPES = "profile email openid";
 const TOKEN = "Test token";
+const ENCODED_TOKEN = "1.IlRlc3QgdG9rZW4i";
+const tokenConsumer = jest.fn();
 const authService = new AuthenticationHandlerService({
   retrieveToken: jest.fn().mockReturnValue(TOKEN),
 });
@@ -19,45 +26,80 @@ const failingAuthService = new AuthenticationHandlerService({
 });
 const successfulGrantOffileAccess = jest.fn().mockReturnValue({code: "123"});
 
-it("returns the correct string if successful", async () => {
+it("returns the correct string if sign in successful", async () => {
   authInstance.grantOfflineAccess = successfulGrantOffileAccess;
 
-  let token = await authService.signIn(EXPECTED_SCOPES);
+  await authService.signIn(EXPECTED_SCOPES, tokenConsumer);
 
-  expect(token).toBe(TOKEN);
+  expect(authInstance.isSignedIn.listen).toHaveBeenCalled();
 });
 
-it("throws error if grantOffileAccess returns undefined", async () => {
+it("throws error if grantOffileAccess returns undefined in sign in", async () => {
   authInstance.grantOfflineAccess = jest.fn().mockReturnValue(undefined);
 
-  await expect(authService.signIn(EXPECTED_SCOPES))
+  await expect(authService.signIn(EXPECTED_SCOPES, tokenConsumer))
   .rejects
   .toThrow(FailureToSignInError);
 });
 
-it("throws error if grantOffileAccess throws error", async () => {
+it("throws error if grantOffileAccess throws error in sign in", async () => {
   authInstance.grantOfflineAccess = jest.fn().mockImplementation(() => {
     throw new Error();
   });
 
-  await expect(authService.signIn(EXPECTED_SCOPES))
+  await expect(authService.signIn(EXPECTED_SCOPES, tokenConsumer))
   .rejects
   .toThrow(FailureToSignInError);
 });
 
-it("throws error if backend API throws error", async () => {
+it("throws error if backend API throws error in sign in", async () => {
   authInstance.grantOfflineAccess = successfulGrantOffileAccess;
 
-  await expect(failingAuthService.signIn(EXPECTED_SCOPES))
+  await expect(failingAuthService.signIn(EXPECTED_SCOPES, tokenConsumer))
   .rejects
   .toThrow(FailureToSignInError);
 });
 
-it("signs out the user", async () => {
-  authInstance.grantOfflineAccess = successfulGrantOffileAccess;
-  await authService.signIn(EXPECTED_SCOPES);
+it("signs out the user if they're signed in", async () => {
+  await authService.signIn(EXPECTED_SCOPES, tokenConsumer);
+  authInstance.isSignedIn.get = jest.fn().mockReturnValue(true);
 
-  authService.signOut().then(success => {
-    expect(success).toBe(true);
-  });
+  const success = await authService.signOut();
+  expect(success).toBe(true);
+});
+
+it("returns false when signing out a signed out user", async () => {
+  await authService.signIn(EXPECTED_SCOPES, tokenConsumer);
+  authInstance.isSignedIn.get = jest.fn().mockReturnValue(false);
+
+  const success = await authService.signOut();
+  expect(success).toBe(false);
+});
+
+it("gets a user's login status if they're logged in", async () => {
+  authInstance.isSignedIn.get = jest.fn().mockReturnValue(true);
+  authService.getToken = jest.fn().mockReturnValue(TOKEN);
+
+  const success = authService.getUserLoginStatus();
+  expect(success).toBe(true);
+});
+
+it("gets a user's login status if they're logged out", async () => {
+  authInstance.isSignedIn.get = jest.fn().mockReturnValue(false);
+  authService.getToken = jest.fn().mockReturnValue(TOKEN);
+  
+  const success = authService.getUserLoginStatus();
+  expect(success).toBe(false);
+});
+
+it("parses a token if one exists", async () => {
+  authService.getToken = jest.fn().mockReturnValue(ENCODED_TOKEN);
+  const result = authService.getParsedToken();
+  expect(result).toBe(TOKEN);
+});
+
+it("returns an empty object if no parsed token exists", async () => {
+  authService.getToken = jest.fn().mockReturnValue(undefined);
+  const result = authService.getParsedToken();
+  expect(result).toStrictEqual({});
 });
--- frontend/src/services/authentication_handler_service/authentication_handler_service.ts
+++ frontend/src/services/authentication_handler_service/authentication_handler_service.ts
@@ -1,5 +1,4 @@
 import { BackendAuthenticationInterface } from "../backend_service_interface/backend_service_interface";
-import { CLIENT_ID } from "./authentication_constants";
 
 /** Error that occurs if sign in fails. */
 export class FailureToSignInError extends Error {
@@ -12,34 +11,67 @@ export class FailureToSignInError extends Error {
 }
 
 /**
- * Communicates with gapi to retrieve auth code from user.
- * Exchanges auth code and redirect URI for ID token from backend.
+ * Communicates with gapi to manage a user's login status
+ * and retrieve auth codes / tokens
  */
 export class AuthenticationHandlerService {
+  token: string | undefined = undefined;
+  authInstance = undefined;
+
   /** Backend is responsible for retrieving the ID token. */
-  constructor(private readonly backend: BackendAuthenticationInterface) {
-    // Safe because the gapi script is loaded in the head with no async/defer.
-    // TODO: Make gapi script async and call this when it has loaded
-    window.gapi.load('auth2', () => {
-      window.gapi.auth2.init({
-        client_id: CLIENT_ID,
-      });
-    });
+  constructor(private readonly backend: BackendAuthenticationInterface) {}
+
+  /**
+   * Determines if a user is logged in.
+   * @return true if user is logged in, false otherwise
+   */
+  getUserLoginStatus(): boolean {
+    const auth = this.getAuthInstance()
+    return auth.isSignedIn.get() && this.getToken() !== undefined;
   }
 
   /**
-   * Signs the user in, retrieving a token from the backend.
+   * Gets the parsed ID token of the user.
+   * @return the parsed token if it exists, otherwise the empty object
+   */
+  getParsedToken() {
+    const token = this.getToken();
+    return token
+        ? JSON.parse(atob(token.split(".")[1]))
+        : {};
+  }
+
+  /**
+   * Gets the ID token of the user.
+   * @return the token if it exists, otherwise undefined
+   */
+  getToken(): string | undefined {
+    const auth = this.getAuthInstance()
+    const response = auth.currentUser.get().getAuthResponse()
+    if (!response.id_token) {
+      return;
+    }
+    return response.id_token;
+  }
+
+  /**
+   * Signs the user in, calls a function on the token retrieved from backend.
    * @param scopes the scopes our app requests for oauth
-   * @return the string of the JWT token retrieved by the backend
+   * @param tokenConsumer a function that takes the token as input and is
+   *     called when the token is received from the backend.
    * @throws FailureToSignInError if sign in fails
    */
-  async signIn(scopes: string): Promise<string> {
+  async signIn(scopes: string,
+               tokenConsumer: (token: string) => void): Promise<void> {
     const code: string | undefined = await this.getAuthCode({scope: scopes});
     if (code === undefined) {
       throw new FailureToSignInError();
     }
     try {
-      return await this.backend.retrieveToken(code, window.location.origin);
+      const redirectUri = window.location.origin;
+      const token = await this.backend.retrieveToken(code, redirectUri);
+      // Bind the token consumer to be called when Auth2 says user logged in.
+      this.oneTimeBindToSignIn(token, tokenConsumer)
     } catch (err) {
       throw new FailureToSignInError();
     }
@@ -50,20 +82,39 @@ export class AuthenticationHandlerService {
    * @return false if the user is already signed out, true if successful
    */
   async signOut(): Promise<boolean> {
-    const authInstance = window.gapi.auth2.getAuthInstance();
-    if (!authInstance) {
+    const auth = this.getAuthInstance()
+    if (!auth.isSignedIn.get()) {
       return false;
     }
-    await authInstance.signOut();
-    localStorage.removeItem('token');
+    await auth.signOut();
     return true;
+  }
+
+  // TypeScript 4.0 will introduce variadic generics, until then this works.
+  private oneTimeBindToSignIn<T>(arg: T, func: (a: T) => void) {
+    // Infinite listeners are allowed, this make sure each listener runs once.
+    let called = false;
+    this.getAuthInstance().isSignedIn.listen((signIn: boolean) => {
+      if (signIn && !called) {
+        called = true;
+        func(arg)
+      }
+    });
+  }
+
+  // Lazy initialization of the Auth2 instance singleton.
+  private getAuthInstance() {
+    if (!this.authInstance) {
+      this.authInstance = window.gapi.auth2.getAuthInstance();
+    }
+    return this.authInstance;
   }
 
   private async getAuthCode(
       opt?: gapi.auth2.OfflineAccessOptions): Promise<string | undefined> {
-    const authInstance = window.gapi.auth2.getAuthInstance();
+    const auth = this.getAuthInstance()
     try {
-      const { code } = await authInstance.grantOfflineAccess(opt);
+      const { code } = await auth.grantOfflineAccess(opt);
       return code;
     } catch (err) {
       return;
